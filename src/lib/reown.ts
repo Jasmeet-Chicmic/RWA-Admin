09fd6439a71766e2eac78dcc97a55c004c135c76
sadaf-chicmic <sadaf@chicmicstudios.in>
2026-04-15 18:12:55 +0530

    refactor: replace hardcoded Phantom wallet ID with a constant variable in reown configuration

--- src/lib/reown.ts
+++ src/lib/reown.ts
@@ -10,6 +10,8 @@ export const queryClient = new QueryClient();
 
 const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID;
 const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+const PHANTOM_WALLET_ID =
+  "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393";
 
 const metadata = {
   name: "Townly Admin Panel",
@@ -39,8 +41,6 @@ if (isReownConfigured && wagmiAdapter) {
       socials: false,
       email: false,
     },
-    excludeWalletIds: [
-      "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393", // Phantom
-    ],
+    excludeWalletIds: [PHANTOM_WALLET_ID],
   });
 }
